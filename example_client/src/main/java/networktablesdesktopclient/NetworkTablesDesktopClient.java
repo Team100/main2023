d46da8711532521968e9efc9dfd90d40282d6a21
Joel Truher <joel@truher.org>
2022-09-16 08:28:21 -0700

    correctly show networktables array

--- example_client/src/main/java/networktablesdesktopclient/NetworkTablesDesktopClient.java
+++ example_client/src/main/java/networktablesdesktopclient/NetworkTablesDesktopClient.java
@@ -1,5 +1,6 @@
 package networktablesdesktopclient;
 
+import edu.wpi.first.networktables.EntryInfo;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
@@ -11,10 +12,9 @@ public class NetworkTablesDesktopClient {
 
   public void run() {
     NetworkTableInstance inst = NetworkTableInstance.getDefault();
+    inst.startClient("wpilibpi.local");
     NetworkTable table = inst.getTable("Vision");
     NetworkTableEntry idEntry = table.getEntry("id");
-    //inst.startClientTeam(294);
-    inst.startClient("wpilibpi.local");
     while (true) {
       try {
         Thread.sleep(1000);
@@ -22,8 +22,9 @@ public class NetworkTablesDesktopClient {
         System.out.println("interrupted");
         return;
       }
-      double id = idEntry.getDouble(0.0);
-      System.out.println("id: " + id);
+      for(double i: idEntry.getDoubleArray(new double[]{0})) {
+        System.out.println("id: " + i);
+      }
     }
   }
 }
