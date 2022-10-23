1290ffd1878d23ab93621d4065009a4f03783842
Joel Truher <joel@truher.org>
2022-10-22 20:55:57 -0700

    fiddle with apriltags for awhile

--- apriltags_example/app.py
+++ apriltags_example/app.py
@@ -7,83 +7,178 @@ import numpy as np
 import time
 import draw
 from pupil_apriltags import Detector
-
+from io import BytesIO
+from picamera import PiCamera
 
 def main():
-    with open("/boot/frc.json") as f:
-        config = json.load(f)
-    camera_config = config["cameras"][0]
+    print('main initiated')
+    #input_stream = BytesIO()
+#1	1920x1080	16:9	1/10 <= fps <= 30	x	 	Partial	None
+#2	3280x2464	4:3	1/10 <= fps <= 15	x	x	Full	None
+#3	3280x2464	4:3	1/10 <= fps <= 15	x	x	Full	None
+#4	1640x1232	4:3	1/10 <= fps <= 40	x	 	Full	2x2
+#5	1640x922	16:9	1/10 <= fps <= 40	x	 	Full	2x2
+#6	1280x720	16:9	40 < fps <= 90	x	 	Partial	2x2
+#7	640x480	4:3	40 < fps <= 90	x	 	Partial	2x2
+    #width = 1280
+    #height = 720
+    # mode 4 is full frame binned
+    #width = 1640
+    #height = 1232
+    # mode 5 is full width -- we kinda don't need height, we need width, so mode 5 is ok
+    # note 1648/928 is rounded up
+    #width = 1664
+    #height = 928
 
-    width = camera_config["width"]
-    height = camera_config["height"]
+# this goes 40fps
+    width = 832
+    height = 464
+
+    # somewhat calibrated using the 200mm tag
+    # note these are resolution-dependent, see above
+    # this is for mode 1 at 720
+    #camera_params = [1400, 1400, width / 2, height / 2]
+    # this is for mode 5 at 928
+    camera_params = [500, 500, width / 2, height / 2]
+
+    camera = PiCamera(resolution=(width, height),
+#    framerate = 28,
+    framerate = 40, # max for mode 5
+    # mode 1 is cropped severely, we don't want that, we want the full sensor
+    # sensor_mode = 1)
+    # mode 4 uses 2x2 binning, full frame, doesn't seem to work. (!)
+    #sensor_mode = 4)
+    # mode 5 is like 4 but shorter, maybe less data to move around?
+    sensor_mode = 5)
+
+    #camera.shutter_speed = 10000
+    #camera.iso = 800
+#    camera.start_preview()
+    print('camera set up')
+    time.sleep(2)
+    #camera.capture(input_stream, 'jpeg')
 
     cs = CameraServer.getInstance()
-    camera = cs.startAutomaticCapture()
 
-    input_stream = cs.getVideo()
+
     output_stream = cs.putVideo("Processed", width, height)
+    print('stream initiated')
 
-    NetworkTables.initialize()
+    #Roborio IP: 10.1.0.2
+    #Pi IP: 10.1.0.21
+    NetworkTables.initialize(server = '10.1.0.2')
+    print('NetworkTables initialized')
     # Table for vision output information
     vision_nt = NetworkTables.getTable("Vision")
 
     # Allocating new images is very expensive, always try to preallocate
-    img = np.zeros(shape=(240, 320, 3), dtype=np.uint8)
+# try YUV
+    img = np.zeros(shape=(height, width, 3), dtype=np.uint8)
+#    img = np.zeros(shape=(height, width), dtype=np.uint8)
 
     # Wait for NetworkTables to start
     time.sleep(0.5)
 
-    camera_params = [300, 300, width / 2, height / 2]
-    tag_size = 0.04
-    at_detector = Detector()
+    # tag sizes
+    # the little cubes = 0.03m or so
+    # big ones on a whole sheet of paper = about 0.2m
+    tag_size = 0.2
 
+    # threads don't help which is weird.
+    # quad_decimate only helps a little, hurts range
+    #at_detector = Detector(quad_decimate=1.0)
+    at_detector = Detector()
+    print('last pre-loop')
     while True:
         start_time = time.time()
-        frame_time, input_img = input_stream.grabFrame(img)
+        #print(start_time)
+        #input_img = input_stream.read()
 
-        # Notify output of error and skip iteration
-        if frame_time == 0:
-            output_stream.notifyError(input_stream.getError())
-            continue
+        # notify output of error and skip iteration
+        #if input_stream.tell() == 0:
+            #output_stream.notifyError(input_stream.getError())
+            #continue
+# try YUV
+        camera.capture(img, 'bgr', use_video_port = True)
 
-        gray_image = cv2.cvtColor(input_img, cv2.COLOR_BGR2GRAY)
+# the docs say to specify a too-small buffer and it will just work
+
+#        try:
+#            camera.capture(img, 'yuv', use_video_port = True)
+#        except IOError as e:
+#            pass
+#        img = img[:height, :width]
+
+        #print('camera capture')
+
+        gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+#        gray_image = img
+
         result = at_detector.detect(
             gray_image,
             estimate_tag_pose=True,
             camera_params=camera_params,
             tag_size=tag_size,
         )
+        #print('image converted')
 
-        output_img = np.copy(input_img)
-        draw.draw_result(output_img, camera_params, tag_size, result)
+        #output_img = np.copy(img)
+        #draw.draw_result(output_img, camera_params, tag_size, result)
+
+        #output_img = np.copy(img)
+        draw.draw_result(img, camera_params, tag_size, result)
+        #print('result draw')
 
         id_list = []
         pose_t_x_list = []
         pose_t_y_list = []
         pose_t_z_list = []
+        pose_R_x_list = []
+        pose_R_y_list = []
+        pose_R_z_list = []
 
         for r in result:
             id_list.append(r.tag_id)
+            ap_pose_R_x_str = ''
+            ap_pose_R_y_str = ''
+            ap_pose_R_z_str = ''
+            for i in range(3):
+                ap_pose_R_x_str = ap_pose_R_x_str + str(round(r.pose_R[i][0], 4))
+                ap_pose_R_y_str = ap_pose_R_y_str + str(round(r.pose_R[i][1], 4))
+                ap_pose_R_z_str = ap_pose_R_z_str + str(round(r.pose_R[i][2], 4))
+                if i != 2:
+                    ap_pose_R_x_str = ap_pose_R_x_str + ', '
+                    ap_pose_R_y_str = ap_pose_R_y_str + ', '
+                    ap_pose_R_z_str = ap_pose_R_z_str + ', '
             pose_t_x_list.append(r.pose_t[0])
             pose_t_y_list.append(r.pose_t[1])
             pose_t_z_list.append(r.pose_t[2])
+            pose_R_x_list.append(ap_pose_R_x_str)
+            pose_R_y_list.append(ap_pose_R_y_str)
+            pose_R_z_list.append(ap_pose_R_z_str)
 
         vision_nt.putNumberArray("id", id_list)
         vision_nt.putNumberArray("pose_t_x", pose_t_x_list)
         vision_nt.putNumberArray("pose_t_y", pose_t_y_list)
         vision_nt.putNumberArray("pose_t_z", pose_t_z_list)
+        vision_nt.putStringArray("pose_R_x", pose_R_x_list)
+        vision_nt.putStringArray("pose_R_y", pose_R_y_list)
+        vision_nt.putStringArray("pose_R_z", pose_R_z_list)
 
         processing_time = time.time() - start_time
         fps = 1 / processing_time
         cv2.putText(
-            output_img,
+            #output_img,
+            img,
             f"fps: {str(round(fps, 1))}",
             (0, 20),
             cv2.FONT_HERSHEY_SIMPLEX,
             0.5,
             (255, 255, 255),
         )
-        output_stream.putFrame(output_img)
+        #output_stream.putFrame(output_img)
+        output_stream.putFrame(img)
+        #print('loop finish')
 
 
 main()
