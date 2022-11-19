ac12c878bef3e0e33e86eb9a2bf2c37f94c2e220
Ishan Harbury <98669601+812405@users.noreply.github.com>
2022-11-19 11:15:47 -0800

    converted to 2023

--- LocalizationTest/src/main/java/frc/robot/AprilTag.java
+++ LocalizationTest/src/main/java/frc/robot/AprilTag.java
@@ -2,17 +2,17 @@ package frc.robot;
 
 import edu.wpi.first.math.geometry.Pose2d;
 
-public class TestAprilTag {
+public class AprilTag {
     
     public int ID;
     public Pose2d Pose2d;
     
-    public TestAprilTag(int ID, Pose2d pose) {
+    public AprilTag(int ID, Pose2d pose) {
         this.ID = ID;
         this.Pose2d = pose;
     }
 
-    public Pose2d getPose(){
+    public Pose2d getPose() {
         return this.Pose2d;
     }
 
--- LocalizationTest/src/main/java/frc/robot/PoseTest.java
+++ LocalizationTest/src/main/java/frc/robot/PoseTest.java
@@ -6,7 +6,7 @@ import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.geometry.Pose2d;
 
 public class PoseTest {
-    public static Pose2d toFieldCoordinates(Translation2d translation2d, Rotation2d rotation2d, TestAprilTag apriltag) {
+    public static Pose2d toFieldCoordinates(Translation2d translation2d, Rotation2d rotation2d, AprilTag apriltag) {
         Transform2d robotRelative = new Transform2d(translation2d, rotation2d);
         Transform2d tagRelative = robotRelative.inverse();
         Pose2d fieldRelative = apriltag.getPose().plus(tagRelative);
