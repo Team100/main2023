bbe368cb4adef34028efe6c4c7f88872c9764914
Joel Truher <joel@truher.org>
2022-12-08 23:02:09 -0800

    network tables 4 with msgpack payloads

---
+++ nt4-msgpack/src/main/java/frc/robot/SomeData.java
@@ -0,0 +1,9 @@
+package frc.robot;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SomeData {
+    public int x = 0;
+    public Map<String, Double> m = new HashMap<String, Double>();
+}
