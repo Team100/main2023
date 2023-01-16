d99996ebc711e68578b0745e85ccff8486030efe
Aidan Sacco <saccoa54@gmail.com>
2023-01-16 00:17:42 -0800

    Revert "Rewrite swerve code"
    This reverts commit c907ac0e66007ea42b3caf584168c1feac846761.

--- swerve2/src/main/java/frc/robot/subsystems/motors/DriveMotor.java
+++
@@ -1,8 +0,0 @@
-package frc.robot.subsystems.motors;
-
-import edu.wpi.first.util.sendable.Sendable;
-
-public interface DriveMotor extends Sendable {
-    double get();
-    void set(double output);
-}
--- swerve2/src/main/java/frc/robot/subsystems/motors/TurningMotor.java
+++
@@ -1,8 +0,0 @@
-package frc.robot.subsystems.motors;
-
-import edu.wpi.first.util.sendable.Sendable;
-
-public interface TurningMotor extends Sendable {
-	double get();
-    void set(double output);
-}
