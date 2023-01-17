15aefebf665012dcac077e62d93eae1cf42a94a6
Joel Truher <joel@truher.org>
2023-01-16 20:34:02 -0800

    moved many things to studies2023

--- nt4-msgpack/src/main/java/frc/robot/SomeData.java
+++
@@ -1,9 +0,0 @@
-package frc.robot;
-
-import java.util.HashMap;
-import java.util.Map;
-
-public class SomeData {
-    public int x = 0;
-    public Map<String, Double> m = new HashMap<String, Double>();
-}
