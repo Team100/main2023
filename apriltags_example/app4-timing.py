837520509cc1b60becae83267eb29b54ad4786de
Joel Truher <joel@truher.org>
2023-01-30 10:46:42 -0800

    a few performance ideas

--- apriltags_example/app4-timing.py
+++ apriltags_example/app4-timing.py
@@ -2,275 +2,95 @@
 # pylint: disable=missing-function-docstring
 # pylint: disable=missing-class-docstring
 
-import io
-import json
+"""
+app4-timing.py
+
+This is a standalone app for the WPILibPi vision system, specifically
+to investigate video pipeline latency, separate from any video
+interpretation logic.
+
+To use it, navigate to the "Application" tab of the WPILibPi webserver,
+click "Writable" at the top, choose "Uploaded Python File," choose this
+file and clikc "Upload and Save."
+"""
+
 import time
-
-
-import RPi.GPIO as GPIO           # import RPi.GPIO module  
-
-
-import cv2
-import msgpack
 import numpy as np
-from dataclasses import dataclass, field
-from typing import List
+import libcamera
+from RPi import GPIO
 from cscore import CameraServer
 from ntcore import NetworkTableInstance
 from picamera2 import MappedArray
 from picamera2 import Picamera2
-from pupil_apriltags import Detector
-
-global_frame_time = time.time()
-
-@dataclass
-class Tag:
-    id: int
-    pose_t: List[float]
-    pose_R: List[float]
-
-@dataclass
-class Tags:
-    et: float = 0
-   # tags: List[Tag] = field(default_factory=list)
 
 
-
-
-class TagFinder:
+class LEDFinder:
     def __init__(self, width, height):
-        self.frame_time = time.time()
         self.width = width
         self.height = height
 
-        # NetworkTables Rio as server
         inst = NetworkTableInstance.getDefault()
         inst.startClient4("app4")
-        inst.setServer("10.1.0.2")
-        
-        # NetworkTables.initialize(server="10.1.0.14")
-        # NetworkTables Pi as server (for testing)
-        # NetworkTables.initialize()
- 
-        # server mode for testing
-        # inst.startServer()
+        inst.setServer("10.1.0.2")  # RoboRIO IP
 
         # Table for vision output information
         self.vision_nt = inst.getTable("Vision")
- 
-        self.vision_nt_id = self.vision_nt.getDoubleArrayTopic("id").publish()
-        self.vision_nt_tx = self.vision_nt.getDoubleArrayTopic("pose_t_x").publish()
-        self.vision_nt_ty = self.vision_nt.getDoubleArrayTopic("pose_t_y").publish()
-        self.vision_nt_tz = self.vision_nt.getDoubleArrayTopic("pose_t_z").publish()
-        self.vision_nt_rx = self.vision_nt.getStringArrayTopic("pose_R_x").publish()
-        self.vision_nt_ry = self.vision_nt.getStringArrayTopic("pose_R_y").publish()
-        self.vision_nt_rz = self.vision_nt.getStringArrayTopic("pose_R_z").publish()
-        self.vision_nt_msgpack = self.vision_nt.getRawTopic("tags").publish("msgpack")
         self.vision_nt_led = self.vision_nt.getBooleanTopic("led").publish()
 
-        self.tag_size = 0.2
-        self.circle_tag_size = 0.8
-        self.at_detector = Detector(families="tag16h5")
-        self.at_circle_detector = Detector(families="tagCircle21h7")
         # self.output_stream = CameraServer.putVideo("Processed", width, height)
         # vertical slice
-        # TODO: one slice for squares one for circles
-        self.output_stream = CameraServer.putVideo("Processed", width, int(height/2))
+        self.output_stream = CameraServer.putVideo(
+            "Processed", self.width, int(self.height / 2)
+        )
         self.camera_params = [
             357.1,
             357.1,
-            width / 2,
-            height / 2,
+            self.width / 2,
+            self.height / 2,
         ]
 
-    def pre_callback(self, request):
-        with MappedArray(request, "lores") as m:
-            self.analyze(m.array, self.width, self.height)
+    # the callback blocks the camera thread, so don't do that.
+    #    def pre_callback(self, request):
+    #        with MappedArray(request, "lores") as mapped_array:
+    #            self.analyze(mapped_array.array)
 
-    def analyze(self, data, width, height):
-        start_time = time.time()
-
-        y_len = width * height
+    def analyze(self, buffer):
+        y_len = self.width * self.height
         # truncate, ignore chrominance
-        img = np.frombuffer(data, dtype=np.uint8, count=y_len)
-        img = img.reshape((height, width))
+        # this makes a view, takes 300ns
+        img = np.frombuffer(buffer, dtype=np.uint8, count=y_len)
+        # this also makes a view, takes 150ns
+        img = img.reshape((self.height, self.width))
         # slice out the middle, there's never a target near the top or the bottom
         # TODO: one slice for squares one for circles
-        img = img[int(height/4):int(3*height/4), :width]
-        # img = img[:height, :width]
+        # this also makes a view, takes 150ns
+        img = img[int(self.height / 4) : int(3 * height / 4), : self.width]
+        # img = img[:self.height, :self.width]
         # for debugging the size:
-        # img = np.frombuffer(data, dtype=np.uint8)
+        # img = np.frombuffer(buffer, dtype=np.uint8)
         # img = img.reshape((int(height*3/2), -1))
         # img = img[: height, ]
-        # print(img.shape) # (width,height) = (616,832)
-
+        # print(img.shape) # (self.width,self.height) = (616,832)
         # print("MIN", np.amin(img), "MAX", np.amax(img))
 
-
-        result = []
-
-      #  result = self.at_detector.detect(
-     #       img,
-      #      estimate_tag_pose=True,
-     #       camera_params=self.camera_params,
-    #        tag_size=self.tag_size,
-   #     )
-
-    #    circle_result = self.at_circle_detector.detect(
-   #         img,
-     #       estimate_tag_pose=True,
-    #        camera_params=self.camera_params,
-   #         tag_size=self.circle_tag_size,
-  #      )
-
-   #     result = result + circle_result
-
-     #   self.draw_result(img, result)
-
-        id_list = []
-        pose_t_x_list = []
-        pose_t_y_list = []
-        pose_t_z_list = []
-        pose_r_x_list = []
-        pose_r_y_list = []
-        pose_r_z_list = []
-
-#        tags = Tags()
-        tags = {}
-        tags['tags'] = []
-
-
-        good_result_count = 0
-
-        for result_item in result:
-            if result_item.hamming > 0:
-                continue
-
-            good_result_count += 1
-
-            tags['tags'].append({
-                'id': result_item.tag_id, 
-                'pose_t': result_item.pose_t.tolist(),
-                'pose_R': result_item.pose_R.tolist()
-                })
-
-            id_list.append(result_item.tag_id)
-            ap_pose_r_x_str = ""
-            ap_pose_r_y_str = ""
-            ap_pose_r_z_str = ""
-
-            if result_item.pose_R is not None:
-                for i in range(3):
-                    ap_pose_r_x_str = (
-                        ap_pose_r_x_str + str(round(result_item.pose_R[0][i], 4)) + " "
-                    )
-                    ap_pose_r_y_str = (
-                        ap_pose_r_y_str + str(round(result_item.pose_R[1][i], 4)) + " "
-                    )
-                    ap_pose_r_z_str = (
-                        ap_pose_r_z_str + str(round(result_item.pose_R[2][i], 4)) + " "
-                    )
-
-            if result_item.pose_t is not None:
-                pose_t_x_list.append(result_item.pose_t[0])
-                pose_t_y_list.append(result_item.pose_t[1])
-                pose_t_z_list.append(result_item.pose_t[2])
-
-            pose_r_x_list.append(ap_pose_r_x_str)
-            pose_r_y_list.append(ap_pose_r_y_str)
-            pose_r_z_list.append(ap_pose_r_z_str)
-
-#        if good_result_count > 0:
-#            GPIO.output(37, 1)
-#        else:
-#            GPIO.output(37, 0)
- #       led_on = np.amax(img) > 200
- #       #print(led_on)
-  #      if (led_on):
-  #          GPIO.output(37, 1)
-  #      else:f
-  #          GPIO.output(37, 0)
-
-
-   #     self.vision_nt_id.set(id_list)
-  #      self.vision_nt_tx.set(pose_t_x_list)
-  #      self.vision_nt_ty.set(pose_t_y_list)
-   #     self.vision_nt_tx.set(pose_t_z_list)
-   #     self.vision_nt_rx.set(pose_r_x_list)
-    #    self.vision_nt_ry.set(pose_r_y_list)
-    #    self.vision_nt_rz.set(pose_r_z_list)
-
-        current_time = time.time()
-        analysis_et = current_time - start_time
-        total_et = current_time - self.frame_time
-
-        tags['et'] = total_et
-        # print(tags)
-        
-        posebytes = msgpack.packb(tags)
-    #    self.vision_nt_msgpack.set(posebytes)
+        # not instant, ~300us
         led_on = np.amax(img) > 200
         self.vision_nt_led.set(led_on)
 
-        fps = 1 / total_et
-     #   self.frame_time = current_time
-    #    self.draw_text(img, f"analysis ET(ms) {1000*analysis_et:.0f}", (5, 25))
-    #    self.draw_text(img, f"total ET(ms) {1000*total_et:.0f}", (5, 65))
-     #   self.draw_text(img, f"fps {fps:.1f}", (5, 105))
-     #   self.output_stream.putFrame(img)
+        # self.output_stream.putFrame(img)
 
-        
-        #print(led_on)
-        if (led_on):
+        # print(led_on)
+        if led_on:
             GPIO.output(37, 1)
         else:
             GPIO.output(37, 0)
-
-    
-   
-    def draw_result(self, image, result):
-        for result_item in result:
-            if result_item.hamming > 0:
-                continue
-            (pt_a, pt_b, pt_c, pt_d) = result_item.corners
-            pt_a = (int(pt_a[0]), int(pt_a[1]))
-            pt_b = (int(pt_b[0]), int(pt_b[1]))
-            pt_c = (int(pt_c[0]), int(pt_c[1]))
-            pt_d = (int(pt_d[0]), int(pt_d[1]))
-
-            cv2.line(image, pt_a, pt_b, (255, 255, 255), 2)
-            cv2.line(image, pt_b, pt_c, (255, 255, 255), 2)
-            cv2.line(image, pt_c, pt_d, (255, 255, 255), 2)
-            cv2.line(image, pt_d, pt_a, (255, 255, 255), 2)
-
-            (c_x, c_y) = (int(result_item.center[0]), int(result_item.center[1]))
-            cv2.circle(image, (c_x, c_y), 10, (255, 255, 255), -1)
-
-            tag_id = result_item.tag_id
-            self.draw_text(image, f"id {tag_id}", (c_x, c_y))
-            tag_family = result_item.tag_family.decode("utf-8")
-            self.draw_text(image, f"id {tag_family}", (c_x, c_y + 40))
-
-            # put the pose translation in the image
-            # the use of 'item' here is to force a scalar to format
-            if result_item.pose_t is not None:
-                self.draw_text(image, f"X {result_item.pose_t.item(0):.2f}m", (c_x, c_y + 80))
-                self.draw_text(image, f"Y {result_item.pose_t.item(1):.2f}m", (c_x, c_y + 120))
-                self.draw_text(image, f"Z {result_item.pose_t.item(2):.2f}m", (c_x, c_y + 160))
-
-    # these are white with black outline
-    def draw_text(self, image, msg, loc):
-        cv2.putText(image, msg, loc, cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 0, 0), 6)
-        cv2.putText(image, msg, loc, cv2.FONT_HERSHEY_SIMPLEX, 1.0, (255, 255, 255), 2)
 
 
 def main():
     print("main")
 
-    
-    GPIO.setmode(GPIO.BOARD)            # choose BCM or BOARD  
-    GPIO.setup(37, GPIO.OUT) # set a port/pin as an output   
- 
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(37, GPIO.OUT)
 
     # full frame, 2x2, to set the detector mode
     fullwidth = 1664  # slightly larger than the detector, to match stride
@@ -290,29 +110,55 @@ def main():
     # height=308
 
     camera = Picamera2()
-    camera_config = camera.create_video_configuration(
-        buffer_count=4,  # no need for extra buffers, dropping frames is fine
+    # don't use video, it tries to process all the frames?
+    # camera_config = camera.create_video_configuration(
+    # no need for extra buffers, dropping frames is fine
+    #     buffer_count=4,
+    #     encode="lores",
+    # use single-frame
+    camera_config = camera.create_still_configuration(
+        # one buffer to write, one to read, one in between so we don't have to wait
+        buffer_count=3,
         main={
             "format": "YUV420",
             "size": (fullwidth, fullheight),
         },
         lores={"format": "YUV420", "size": (width, height)},
-        encode="lores",
+        controls={
+            # fast shutter means more gain
+            "AnalogueGain": 8.0,
+            # the flashing LED makes the Auto-Exposure freak out, so turn it off:
+            "ExposureTime": 5000,
+            # go as fast as possible but no slower than 30fps
+            "FrameDurationLimits": (100, 33333),
+            # noise reduction takes time
+            "NoiseReductionMode": libcamera.controls.draft.NoiseReductionModeEnum.Off,
+        },
     )
-    camera.set_controls({"ExposureTime": 5000, "AnalogueGain": 8.0})
     print(camera_config)
     camera.configure(camera_config)
 
     # Roborio IP: 10.1.0.2
     # Pi IP: 10.1.0.21
 
-    output = TagFinder(width, height)
-    camera.pre_callback = output.pre_callback
+    output = LEDFinder(width, height)
+    # the callback blocks the camera thread, so don't do that.
+    # camera.pre_callback = output.pre_callback
     camera.start()
     try:
         while True:
-            time.sleep(1)
+            # the most recent frame, maybe from the past
+            request = camera.capture_request()
+            try:
+                output.analyze(request.make_buffer("lores"))
+            finally:
+                # the frame is owned by the camera so remember to release it
+                request.release()
+            # time.sleep(1)
     finally:
-        camera.stop_recording()
+        # not in video mode
+        # camera.stop_recording()
+        camera.stop()
+
 
 main()
