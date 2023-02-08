5d0abf0c2273f1356b1e563130b2b17df9529f6d
Joel Truher <joel@truher.org>
2023-02-08 10:17:21 -0800

    comment

--- swerve100/src/main/java/frc/robot/subsystems/TurningEncoder.java
+++ swerve100/src/main/java/frc/robot/subsystems/TurningEncoder.java
@@ -4,8 +4,7 @@ import edu.wpi.first.util.sendable.Sendable;
 
 public interface TurningEncoder extends Sendable {
     /**
-     * 
-     * @return module azimuth angle in radians
+     * @return module azimuth angle in radians, counterclockwise-positive
      */
     double getAngle();
     void reset();
