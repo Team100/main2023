2bf596b07b3b31382ea1882db6ab5930e1ade769
Joel Truher <joel@truher.org>
2023-02-13 11:23:02 -0800

    add simple slew rate limiter

--- swerve100/src/main/java/team100/control/DualXboxControl.java
+++ swerve100/src/main/java/team100/control/DualXboxControl.java
@@ -1,5 +1,6 @@
 package team100.control;
 
+import edu.wpi.first.math.filter.SlewRateLimiter;
 import edu.wpi.first.util.sendable.Sendable;
 import edu.wpi.first.util.sendable.SendableBuilder;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -14,16 +15,21 @@ import frc.robot.commands.ResetPose;
  * https://docs.google.com/document/d/1M89x_IiguQdY0VhQlOjqADMa6SYVp202TTuXZ1Ps280/edit#
  */
 public class DualXboxControl implements Control, Sendable {
+    // TODO: express these limits in m/s.
+    private static final int ySlewRateLimit = 3;
+    private static final int xSlewRateLimit = 3;
+    
     private final CommandXboxController controller0;
     private final CommandXboxController controller1;
 
+    private final SlewRateLimiter m_xspeedLimiter = new SlewRateLimiter(xSlewRateLimit);
+    private final SlewRateLimiter m_yspeedLimiter = new SlewRateLimiter(ySlewRateLimit);
+
     public DualXboxControl() {
         controller0 = new CommandXboxController(0);
-        System.out.printf("Controller0: %s\n",
-                controller0.getHID().getName());
+        System.out.printf("Controller0: %s\n", controller0.getHID().getName());
         controller1 = new CommandXboxController(1);
-        System.out.printf("Controller1: %s\n",
-                controller1.getHID().getName());
+        System.out.printf("Controller1: %s\n", controller1.getHID().getName());
         SmartDashboard.putData("Robot Container", this);
     }
 
@@ -58,12 +64,12 @@ public class DualXboxControl implements Control, Sendable {
 
     @Override
     public double xSpeed() {
-        return -1.0 * controller0.getRightY();
+        return m_xspeedLimiter.calculate(-1.0 * controller0.getRightY());
     }
 
     @Override
     public double ySpeed() {
-        return -1.0 * controller0.getRightX();
+        return m_yspeedLimiter.calculate(-1.0 * controller0.getRightX());
     }
 
     @Override
