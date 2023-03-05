e948081a54e67ac71e275fb6ecd096ed8bed4457
Joel Truher <joel@truher.org>
2023-03-04 21:21:18 -0800

    fixed up ishan's malformed spline test

--- swerve100/src/test/java/frc/robot/TestMalformedSpline.java
+++ swerve100/src/test/java/frc/robot/TestMalformedSpline.java
@@ -1,5 +1,7 @@
 package frc.robot;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import java.io.IOException;
 import java.util.List;
 
@@ -7,23 +9,19 @@ import org.junit.jupiter.api.Test;
 
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.trajectory.Trajectory;
 import edu.wpi.first.math.trajectory.TrajectoryConfig;
 import edu.wpi.first.math.trajectory.TrajectoryGenerator;
 import edu.wpi.first.math.trajectory.TrajectoryParameterizer.TrajectoryGenerationException;
 
-public class malformedSplineTest {
+public class TestMalformedSpline {
     @Test
     public void malformedSplineTest() throws IOException {
-        try {
-            Trajectory malformed = TrajectoryGenerator.generateTrajectory(
-            new Pose2d(0, 0, new Rotation2d()),
-            List.of(),
-            new Pose2d(0, 0, new Rotation2d()),
-            new TrajectoryConfig(6, 3)
-            );
-        } catch(TrajectoryGenerationException e) {
-            
-        }
+        assertThrows(TrajectoryGenerationException.class,
+                () -> TrajectoryGenerator.generateTrajectory(
+                        new Pose2d(0, 0, new Rotation2d()),
+                        List.of(),
+                        new Pose2d(0, 0, new Rotation2d()),
+                        new TrajectoryConfig(6, 3)));
+
     }
 }
