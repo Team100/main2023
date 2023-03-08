1e575741ccd62f861a944c42f43ed53939f42b94
Joel Truher <joel@truher.org>
2023-03-08 08:21:36 -0800

    fix test

--- swerve100/src/test/java/frc/robot/goalTest.java
+++ swerve100/src/test/java/frc/robot/goalTest.java
@@ -14,18 +14,19 @@ public class goalTest {
 
     @Test
     public void testRedSubstation() throws IOException {
-        AprilTagFieldLayoutWithCorrectOrientation m_layout =  AprilTagFieldLayoutWithCorrectOrientation.redLayout();
+        AprilTagFieldLayoutWithCorrectOrientation m_layout = AprilTagFieldLayoutWithCorrectOrientation.redLayout();
         Pose2d m_goal = DriveToAprilTag.goal(5, 1, m_layout);
         assertEquals(m_goal.getX(), 15.18, .001);
-        assertEquals(m_goal.getY(), 1.26, .005);
+        assertEquals(m_goal.getY(), 1.46, .005);
         assertEquals(m_goal.getRotation().getRadians(), 0, 0.001);
     }
+
     @Test
     public void testBlueSubstation() throws IOException {
-        AprilTagFieldLayoutWithCorrectOrientation m_layout =  AprilTagFieldLayoutWithCorrectOrientation.blueLayout();
+        AprilTagFieldLayoutWithCorrectOrientation m_layout = AprilTagFieldLayoutWithCorrectOrientation.blueLayout();
         Pose2d m_goal = DriveToAprilTag.goal(4, 1, m_layout);
         assertEquals(m_goal.getX(), 15.18, .01);
-        assertEquals(m_goal.getY(), 6.75, .005);
+        assertEquals(m_goal.getY(), 6.95, .005);
         assertEquals(m_goal.getRotation().getRadians(), 0, 0.001);
     }
 
