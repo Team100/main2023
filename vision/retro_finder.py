4910f7bb6ee80be720d032adaa9803c20fb7b5a1
812405 <812405@seq.org>
2023-03-14 17:05:32 -0700

    retro_finder initial version

---
+++ vision/retro_finder.py
@@ -0,0 +1,37 @@
+import cv2 as cv
+import numpy as np
+import imutils
+import msgpack
+
+
+class RetroFinder:
+
+    def __init__(self, width, height, threshold, threshmode):
+        self.threshold = threshold
+        self.threshmode = threshmode
+        self.metersPerPixel = 1
+        self.horizontal_offset = width/2
+        self.vertical_offset = height/2
+        # TODO: tune this value
+
+    def find(self, img):
+        _, thresh = cv.threshold(img, self.threshold, 255, self.threshmode)
+        contours, hierarchy = cv.findContours(
+            thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+        cnts = imutils.grab_contours(contours)
+
+        tapes = {}
+        tapes["tapes"] = []
+
+        for c in cnts:
+            mmnts = cv.moments(c)
+            cX = (int(mmnts["m10"] / mmnts["m00"]) -
+                  self.horizontal_offset)*self.metersPerPixel
+            cY = (int(mmnts["m01"] / mmnts["m00"]) -
+                  self.vertical_offset)*self.metersPerPixel
+            tapes["tapes"].append(
+                {
+                    "centroid": [cX, cY]
+                }
+            )
+        return tapes
