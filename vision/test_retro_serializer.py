7af8323fe6d2e4a72a02f610d41c0d17cf830105
Joel Truher <joel@truher.org>
2023-03-29 08:55:07 -0700

    examples of unit-testing serialization

---
+++ vision/test_retro_serializer.py
@@ -0,0 +1,30 @@
+# pylint: disable=missing-class-docstring,missing-function-docstring,missing-module-docstring,invalid-name
+import unittest
+import numpy as np
+import retro_serializer
+
+
+class FakeResult:
+    def __init__(self) -> None:
+        self.pose_t = np.array([[1], [2], [3]], dtype=np.float32)
+
+
+class TestRetroSerializer(unittest.TestCase):
+    def test_serialize_empty(self):
+        result = []
+        serialized_result = retro_serializer.serialize(result)
+        self.assertEqual("81a5746170657390", serialized_result.hex())
+
+    def test_serialize_normal(self):
+        result = []
+        result.append(FakeResult())
+        serialized_result = retro_serializer.serialize(result)
+        # this is just copied from whatever the serializer produced.
+        self.assertEqual(
+            (
+                "81a574617065739181a6706f73655f74"
+                "9391cb3ff000000000000091cb400000"
+                "000000000091cb4008000000000000"
+            ),
+            serialized_result.hex(),
+        )
