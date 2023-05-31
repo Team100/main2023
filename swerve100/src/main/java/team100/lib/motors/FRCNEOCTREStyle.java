950a28594c301a660955916c502e0c18bac5cc8b
Joel Truher <joel@truher.org>
2023-05-31 16:48:59 -0700

    remove unnecessary copyright comments

--- swerve100/src/main/java/team100/lib/motors/FRCNEOCTREStyle.java
+++ swerve100/src/main/java/team100/lib/motors/FRCNEOCTREStyle.java
@@ -1,14 +1,7 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
-
 package team100.lib.motors;
 
 import com.ctre.phoenix.motorcontrol.InvertType;
-import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.poenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.sensors.SensorVelocityMeasPeriod;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.SparkMaxLimitSwitch;
