87502e8a778e0601bfc2926f10d27b6ec7bff411
Joel Truher <joel@truher.org>
2023-06-24 15:53:45 -0700

    fix the build

--- swerve100/src/main/java/org/team100/frc2023/LQRManager.java
+++ swerve100/src/main/java/org/team100/frc2023/LQRManager.java
@@ -2,10 +2,8 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot;
+package org.team100.frc2023;
 
-import edu.wpi.first.math.Matrix;
-import edu.wpi.first.math.VecBuilder;
 import edu.wpi.first.math.controller.LinearQuadraticRegulator;
 import edu.wpi.first.math.estimator.KalmanFilter;
 import edu.wpi.first.math.numbers.N1;
